4a5c14409a5154f299aa1985c4620f607cbec91a
Juan Ramirez <juanramirez1776@gmail.com>
2018-12-17 15:31:56 -0800

    added firebase to application

--- chatApp/AppDelegate.swift
+++ chatApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // create manual window
-        
+        FirebaseApp.configure()
         window  = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         
--- chatApp/Controller/ViewController.swift
+++ chatApp/Controller/ViewController.swift
@@ -1,11 +1,14 @@
 
 
 import UIKit
+import Firebase
 
 class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let ref = Database.database().reference(fromURL: "https://primeval-proton-202518.firebaseio.com/")
+        ref.updateChildValues(["someValue": 123])
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
         
     }
