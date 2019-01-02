eb85eaa4a5bf36fa4ac8116bc6697fa7d7a76b13
Juan Ramirez <juanramirez1776@gmail.com>
2019-01-01 19:53:49 -0800

    fileame changes

--- chatApp/Controller/MessageController.swift
+++ chatApp/Controller/MessageController.swift
@@ -3,11 +3,16 @@
 import UIKit
 import Firebase
 
-class ViewController: UITableViewController {
+class MessageController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+        checkIfUserIsLoggedIn()
+    }
+    
+    
+    func checkIfUserIsLoggedIn(){
         // user is not logged in
         if Auth.auth().currentUser?.uid == nil {
             perform(#selector(handleLogout), with: nil, afterDelay: 0)
